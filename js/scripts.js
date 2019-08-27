1fec71aa193821644cc08f2b8a8c50fd3df9fbfc
Chris Burge and Lauren Musante <lauren@>
2019-08-27 14:22:10 -0700

    Add function to show movie details and called function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,44 @@
-function Ticket(movie, date, age, name){
+function Ticket(movie, time, age, name){
   this.movie = movie;
-  this.date = date;
+  this.time = time;
   this.age = age;
   this.name = name;
-  this.price = "";
+  this.price = this.agePrice();
 }
 
-  // Ticket.prototype.calculatePrice = function(age){
-  //   if (this.age <=18){
-  //     this.price = "$5";
-  //
-  //   } else if (this.age > 18 && this.age < 65){
-  //     this.price = "$10";
-  //
-  //   } else {
-  //     this.price = "$6";
-  //   }
-  // }
-
-  Ticket.prototype.calculatePrice = function() {
-    if(this.age < 18) {
-      this.price = 5;
-    } else if(this.age >= 65) {
-      this.price = 6;
+Ticket.prototype.agePrice = function() {
+    if(parseInt(this.age) < 18) {
+      return 5;
+    } else if(parseInt(this.age) >= 65) {
+      return 6;
     } else {
-      this.price = 10
+      return 10;
     }
+  }
+
+  // var Chris = new Ticket ("Flub", "2pm", 38, "Chris")
+  Ticket.prototype.showTicket = function(){
+    return this.name + " " + "you owe" + this.price + " " + "to see" + this.movie;
+  }
+
+  function TicketBundle(ticket, ticketID){
+    this.tickets = [];
 
   }
 
+  TicketBundle.prototype.addTicket = function(ticket) {
+  this.tickets.push(ticket);
+}
 
 
 // UI stuff
 function showTicket(ticketId) {
   $(".ticketDetails").show();
-  $(".yourMovie").html(ticket.movie);
-  $(".yourDate").html(ticket.date);
-  $(".yourAge").html(ticket.age);
-  $(".yourName").html(ticket.name);
-  $(".yourPrice").html(ticket.price);
+  $(".yourMovie").html(ticketId.movie);
+  $(".yourTime").html(ticketId.time);
+  $(".yourAge").html(ticketId.age);
+  $(".yourName").html(ticketId.name);
+  $(".yourPrice").html(ticketId.price);
 }
 
 
@@ -47,10 +47,11 @@ $(document).ready(function(){
   $(".ticketInfo").submit(function(event){
     event.preventDefault();
     var inputtedMovie = $("select[name=movieType] option:selected").val();
-    var inputtedDate = $("input[name=date]").val();
-    var inputtedAge = $("#age").val()
-    var inputtedName = $("#name").val()
+    var inputtedDate = $("select[name=movieTime] option:selected").val();
+    var inputtedAge = $("#age").val();
+    var inputtedName = $("#name").val();
     var newTicket = new Ticket (inputtedMovie, inputtedDate, inputtedAge, inputtedName);
+    showTicket(newTicket);
     $(".ticketDetails").show();
     console.log(inputtedAge);
     console.log(newTicket);
