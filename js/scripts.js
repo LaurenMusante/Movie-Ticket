35ad52f05a7ea2c411f5a2618cce96da4983a84e
Chris Burge and Lauren Musante <lauren@>
2019-08-27 13:23:56 -0700

    add prototype function to calculate ticket price based on age.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,59 @@
-
-// Back End stuff
-function Ticket(movie, time, age){
+function Ticket(movie, date, age, name){
   this.movie = movie;
-  this.time = time;
+  this.date = date;
   this.age = age;
   this.name = name;
-  this.price = function(price){
-    if (this.age <=18{
-      return ("$5")
-    } else if (this.age > 18 && < 65){
-      return ("$10")
-    } else {
-      return ("$6")
-    }
-  }
+  this.price = "";
 }
 
-// var Chris = new Ticket ("Flubber", "8pm", "38")
+  // Ticket.prototype.calculatePrice = function(age){
+  //   if (this.age <=18){
+  //     this.price = "$5";
+  //
+  //   } else if (this.age > 18 && this.age < 65){
+  //     this.price = "$10";
+  //
+  //   } else {
+  //     this.price = "$6";
+  //   }
+  // }
 
+  Ticket.prototype.calculatePrice = function() {
+    if(this.age < 18) {
+      this.price = 5;
+    } else if(this.age >= 65) {
+      this.price = 6;
+    } else {
+      this.price = 10
+    }
 
-
-
-
-
-
-
-
-
+  }
 
 
 
 // UI stuff
+function showTicket(ticketId) {
+  $(".ticketDetails").show();
+  $(".yourMovie").html(ticket.movie);
+  $(".yourDate").html(ticket.date);
+  $(".yourAge").html(ticket.age);
+  $(".yourName").html(ticket.name);
+  $(".yourPrice").html(ticket.price);
+}
+
+
+
 $(document).ready(function(){
+  $(".ticketInfo").submit(function(event){
+    event.preventDefault();
+    var inputtedMovie = $("select[name=movieType] option:selected").val();
+    var inputtedDate = $("input[name=date]").val();
+    var inputtedAge = $("#age").val()
+    var inputtedName = $("#name").val()
+    var newTicket = new Ticket (inputtedMovie, inputtedDate, inputtedAge, inputtedName);
+    $(".ticketDetails").show();
+    console.log(inputtedAge);
+    console.log(newTicket);
 
 
 
@@ -40,9 +62,5 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
+  });
 });
