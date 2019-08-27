937122858da3e90a2ecce35adc628e73068e8929
Chris Burge and Lauren Musante <chris@>
2019-08-27 09:22:45 -0700

    Initialize project and definte first Object

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+
+// Back End stuff
+function Ticket(movie, time, age){
+  this.movie = movie;
+  this.time = time;
+  this.age = age;
+  this.name = {};
+  this.price = function(price){
+    if (this.age <=18{
+      pay 5
+    } else if (this.age > 18 && < 65){
+      pay 10
+    } else {
+      pay 6
+    }
+  }
+}
+
+// var Chris = new Ticket ("Flubber", "8pm", "38")
+
+
+
+
+
+
+
+
+
+
+
+
+
+// UI stuff
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
